058a377eab22affd31c676e463f19fc79e617abe
Juanmi <bodypheo@gmail.com>
2023-05-06 13:11:53 +0200

    Inicio código DB

--- managedb.py
+++ managedb.py
@@ -9,3 +9,11 @@ with eng.begin() as conn:
     salida = conn.execute(text(sql))
     for i in salida:
         print(i[1])
+
+
+# multiple *args no longer accepted, pass a list
+#result = connection.execute(
+#
+#Construir una lista con los resultados a insentar
+#     table.insert(), {"x": 10, "y": 5}, {"x": 15, "y": 12}, {"x": 9, "y": 8}
+#)
---
+++ model/tablas.sql
@@ -0,0 +1,13 @@
+CREATE TABLE urls(
+    url_id INTEGER NOT NULL PRIMARY KEY,
+    url VARCHAR,
+    nombre_app VARCHAR,
+    date_db DATETIME,
+    date_update DATE,
+    date_launch DATE,
+    last_visited DATE,
+    downloads INTEGER
+    description VARCHAR,
+    num_reviews INTEGER,
+    rate FLOAT
+)
