e0b8cbf711b9a1aaef447bf1088226160c97b4aa
Juanmi JF <bodypheo@gmail.com>
2023-05-11 18:12:40 +0200

    Fixing scraping fields

--- scraper.py
+++ scraper.py
@@ -34,22 +34,44 @@ def get_app(url):
         return None
     else:
         title = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div/div[1]/div[1]/div/div/c-wiz/div[2]/div[1]/div/h1/span').text
-
+        #print(title)
     try:
         driver.find_element(By.XPATH, "//button[contains(@class, 'VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ QDwDD mN1ivc VxpoF')]").click()
         print("Botón más encontrado")
         time.sleep(1)
         descargas = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/div[4]/div[2]/div/div/div/div/div[2]/div[3]/div[5]/div[2]').text
-        print(descargas)
+        #print(descargas)
         pub_date = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/div[4]/div[2]/div/div/div/div/div[2]/div[3]/div[8]/div[2]').text
         update_date = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/div[4]/div[2]/div/div/div/div/div[2]/div[3]/div[3]/div[2]').text
     except:
         print("Botón no encontrado")
 
-    time.sleep(2)
-    print(title, "descargas:", descargas, pub_date, update_date)
+    app_prop = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/div[4]/div[2]/div/div/div/div/div[2]/div[3]')
+    props = app_prop.find_elements(By.CLASS_NAME, 'reAt0')
+    #print(len(props))
+    #print('Primer elemento:', props[0].text)
+    for i in range(len(props)):
+        print('Elem: ', i, ' >', props[i].text)
+    
+    if('información' in props[0].text):
+        up_date = props[2].text
+        descargas = props[4].text
+        pub_date = props[7].text
+    else:
+        up_date = props[1].text
+        descargas = props[3].text
+        pub_date = props[8].text
+    #     print('Descargas:', props[i+1].text, 'campo anterior: ', props[i].text)
+    # elif('Última actualización' in props[i].text):
+    #     update_date = props[i+1].text
+    print('Update:', up_date, descargas, pub_date)
     driver.quit
 
 def main():
-    url = 'https://play.google.com/store/apps'
-    get_app(url)
+    #url = 'https://play.google.com/store/apps'
+    url1 = 'https://play.google.com/store/apps/details?id=com.apalon.weatherlive' # app con prop. familiar
+    url2 = 'https://play.google.com/store/apps/details?id=com.coffeemeetsbagel' #app sin prop. familiar
+    get_app(url2)
+
+if __name__== "__main__" :
+    main()
